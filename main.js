4b7250390a77bf878fe36c5f285b82210a0fcd3d
aadyachaturvedi <chaturvedi.aa@northeastern.edu>
2023-02-07 11:36:57 -0500

    basic wireframe and styling done

---
+++ main.js
@@ -0,0 +1,14 @@
+// // clickable button
+// var num = 1;
+// function buttonClicked() {
+
+// 	console.log("button was clicked!")
+
+// 	let newText = "Number of times button clicked:"+ (num++);
+
+// 	// select the button to modify
+// 	let  buttonDiv = document.getElementById("button-div");
+
+// 	buttonDiv.innerHTML = newText;
+
+// }
